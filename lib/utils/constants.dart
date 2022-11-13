bc9ca4fe1b8cdbdd1015b8b4f0009e4a537b4a24
jusylopes <jusylopes@outlook.com>
2022-11-13 20:03:19 -0300

    refactor popular movies

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 abstract class Constants {
   static const String apiKey =
-      'api key';
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMDA3YWMwNzY2ZmU4ZDAyMWIzODlmNThmNjYxZjk5ZSIsInN1YiI6IjYzMjE2ZTUyNWE1ZWQwMDA3ZjE3ZDU3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UAc_zUbpDvaivRiBn7Y0uD-J9xCszuce6A94nKwxl6g';
 
   static const String urlImagePoster =
       'https://image.tmdb.org/t/p/w220_and_h330_face';
