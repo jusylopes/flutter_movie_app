0ad889c9f2f0ea143cbebe3917ec55939065191e
Jusy Lopes <jusylopes@outlook.com>
2023-07-12 23:41:34 -0300

    refactor: repository

--- lib/services/movie_repository.dart
+++ lib/services/movie_repository.dart
@@ -1,19 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:movie_app/models/movie_detail_model.dart';
-import 'package:movie_app/models/popular_movies_model.dart';
-import 'package:movie_app/models/trending_movies_model.dart';
+import 'package:movie_app/models/movie_popular_model.dart';
+import 'package:movie_app/models/movie_trending_model.dart';
+
 import 'package:movie_app/services/api_base_options.dart';
+import 'package:movie_app/utils/strings.dart';
 
 class MovieRepository {
   final Dio _dio = Dio(dioOptions);
 
-  Future<List<PopularMoviesModel>> getPopularMovies({required int page}) async {
+  Future<List<MoviePopularModel>> getPopularMovies({required int page}) async {
     try {
-      Response response = await _dio.get('/movie/popular?page=$page');
+      Response response = await _dio.get('https://api.themoviedb.org/3/movie/popular?api_key=${MovieStrings.apiKey}');
       final data = response.data;
-      List<PopularMoviesModel> movies = data['results']
-          .map<PopularMoviesModel>(
-              (movies) => PopularMoviesModel.fromJson(movies))
+      List<MoviePopularModel> movies = data['results']
+          .map<MoviePopularModel>(
+              (movies) => MoviePopularModel.fromJson(movies))
           .toList();
 
       return movies;
@@ -24,20 +26,20 @@ class MovieRepository {
 
   Future<MovieDetailModel> getMovieDetail({required int id}) async {
     try {
-      Response response = await _dio.get('/movie/$id');
+      Response response = await _dio.get('${MovieStrings.endpointMovieDetail}$id');
       return MovieDetailModel.fromJson(response.data);
     } catch (e) {
       rethrow;
     }
   }
 
-  Future<List<TrendingMoviesModel>> getTrendingMovies() async {
+  Future<List<MovieTrendingModel>> getTrendingMovies() async {
     try {
-      Response response = await _dio.get('/trending/movie/day?');
+      Response response = await _dio.get(MovieStrings.endpointTrendingMovies);
       final data = response.data;
-      List<TrendingMoviesModel> trendingMovies = data['results']
-          .map<TrendingMoviesModel>(
-              (movies) => TrendingMoviesModel.fromJson(movies))
+      List<MovieTrendingModel> trendingMovies = data['results']
+          .map<MovieTrendingModel>(
+              (movies) => MovieTrendingModel.fromJson(movies))
           .toList();
 
       return trendingMovies;
---
+++ lib/utils/strings.dart
@@ -0,0 +1,11 @@
+class MovieStrings {
+  static const String apiKey =
+      'AIzaSyDtQ05s-xuFcFRutlM3KvSCd9sl8wWxjkU';
+//  static const String apiKey = 'apiKey';
+  static const String urlImagePoster =
+      'https://image.tmdb.org/t/p/w220_and_h330_face';
+
+  static String endpointPopularMovies = '/movie/popular?page=';
+  static String endpointMovieDetail = '/movie/';
+  static String endpointTrendingMovies = '/trending/movie/day?';
+}
