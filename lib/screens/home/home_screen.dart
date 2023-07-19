065e0c66b25380398bea0348a83b624bef3916e4
Jusy Lopes <jusylopes@outlook.com>
2023-07-18 22:50:36 -0300

    refactor: home screen

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/screens/trending_movies_page.dart';
+import 'package:movie_app/screens/popular_movies_page.dart';
+import 'package:movie_app/utils/strings.dart';
+
+class HomeSreen extends StatelessWidget {
+  const HomeSreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            height: 40,
+            color: Theme.of(context).primaryColor,
+          ),
+          title: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Text(
+              MovieStrings.appName,
+              style:  Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          bottom: TabBar(
+              labelStyle: Theme.of(context).textTheme.titleSmall,
+              labelColor: Theme.of(context).primaryColor,
+              unselectedLabelColor: Colors.grey,
+              tabs: const [
+                Tab(child: Text('Popular')),
+                Tab(child: Text('Trending'))
+              ]),
+        ),
+        body: const TabBarView(children: [
+          PopularMoviesPage(),
+          TrendingMoviesPage(),
+        ]),
+      ),
+    );
+  }
+}
