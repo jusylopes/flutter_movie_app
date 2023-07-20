7467f7ea879e92c3e63b838ade80790c65a169e7
Jusy Lopes <jusylopes@outlook.com>
2023-07-19 23:26:04 -0300

    refactor: dio options

---
+++ lib/repositories/api_base_options.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+const badeUrlApi = 'https://api.themoviedb.org/3';
+const apikey = '';
+const String endpointPopularMovies = '/movie/popular?page=';
+const String endpointMovieDetail = '/movie/';
+const String endpointTrendingMovies = '/trending/movie/day?';
+
+final dioOptions = BaseOptions(
+    baseUrl: badeUrlApi,
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+    contentType: 'application/json;charset=utf-8',
+    queryParameters: {'api_key': apikey});
