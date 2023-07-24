b56bd4f4220375eebeb8c0f5211d5acc6d242c0d
Jusy Lopes <jusylopes@outlook.com>
2023-07-24 11:18:00 -0300

    add: popular bloc

---
+++ lib/blocs/popular/popular_bloc.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/blocs/popular/popular_event.dart';
+import 'package:movie_app/blocs/popular/popular_state.dart';
+import 'package:movie_app/models/popular_model.dart';
+import 'package:movie_app/repositories/movie_repository.dart';
+
+class PopularMovieBloc extends Bloc<PopularMovieEvent, PopularMovieState> {
+  final MovieRepository _repository;
+  int _currentPage = 1;
+  bool isFetching = false;
+
+  PopularMovieBloc({required MovieRepository<dynamic> repository})
+      : _repository = repository,
+        super(InitialState()) {
+    on<PopularMovieFetchEvent>((event, emit) async {
+      emit(LoadingState());
+      isFetching = true;
+
+      try {
+        final List<MoviePopularModel> response =
+            await _repository.getPopularMovies(page: _currentPage);
+
+        isFetching = false;
+        emit(SuccessState(movies: response));
+        _currentPage++;
+        print(_currentPage);
+        
+      } catch (e) {
+        isFetching = false;
+        emit(ErrorState());
+      }
+    });
+  }
+}
--- lib/screens/popular/popular_screen.dart
+++ lib/screens/popular/popular_screen.dart
@@ -1,35 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app/cubit/popular/popular_cubit.dart';
-import 'package:movie_app/cubit/popular/popular_state.dart';
+import 'package:movie_app/blocs/popular/popular_bloc.dart';
+import 'package:movie_app/blocs/popular/popular_event.dart';
+import 'package:movie_app/blocs/popular/popular_state.dart';
 import 'package:movie_app/screens/widgets/error_message.dart';
-
-import 'package:movie_app/screens/widgets/grid_view_movie.dart';
+import 'package:movie_app/screens/widgets/movie_grid_view.dart';
 
 class PopularMovieScreen extends StatefulWidget {
   const PopularMovieScreen({super.key});
 
   @override
-  State<PopularMovieScreen> createState() => _PopularMovie();
+  State<PopularMovieScreen> createState() => _PopularMovieScreenState();
 }
 
-class _PopularMovie extends State<PopularMovieScreen> {
+class _PopularMovieScreenState extends State<PopularMovieScreen>
+    with AutomaticKeepAliveClientMixin<PopularMovieScreen> {
+  late ScrollController _scrollController;
+  final List _movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    fetchPopularMoviesOnScroll();
+  }
+
+  void fetchPopularMoviesOnScroll() {
+    _scrollController.addListener(() {
+      if (_scrollController.offset ==
+              _scrollController.position.maxScrollExtent &&
+          !BlocProvider.of<PopularMovieBloc>(context).isFetching) {
+        BlocProvider.of<PopularMovieBloc>(context)
+          ..isFetching = true
+          ..add(PopularMovieFetchEvent());
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PopularMovieCubit, PopularMovieState>(
+    super.build(context);
+    return BlocConsumer<PopularMovieBloc, PopularMovieState>(
+      listener: (context, state) {
+        if (state is LoadingState) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text(('Loading movies...'))));
+        }
+      },
       builder: (context, state) {
-        if (state is InitialState || state is LoadingState) {
+        if (state is InitialState || state is LoadingState && _movies.isEmpty) {
           return Center(
             child: CircularProgressIndicator(
                 color: Theme.of(context).primaryColor),
           );
-        } else if (state is SuccessState) {
-          final List movies = state.movies;
-          return GridViewMovie(movie: movies);
-        } else {
+        } else if (state is ErrorState) {
           return const ErrorMessage();
+        } else if (state is SuccessState) {
+          _movies.addAll(state.movies);
+          BlocProvider.of<PopularMovieBloc>(context).isFetching = false;
         }
+
+        return MovieGridView(movie: _movies, controller: _scrollController);
       },
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
